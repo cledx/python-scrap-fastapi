9082af27b4aeed3207680f1f03cd1a84eb079d84
carlos ledoux <carlosledoux@gmail.com>
2026-04-08 16:46:05 +0900

    Daijob Scrapper

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,5 +1,13 @@
 """Scraper logic package."""
 
+from .daijob import DaijobJobListing, parse_daijob_jobs, scrape_daijob_jobs
 from .gaijinpot import GaijinpotJobListing, parse_gaijinpot_jobs, scrape_gaijinpot_jobs
 
-__all__ = ["GaijinpotJobListing", "parse_gaijinpot_jobs", "scrape_gaijinpot_jobs"]
+__all__ = [
+    "DaijobJobListing",
+    "parse_daijob_jobs",
+    "scrape_daijob_jobs",
+    "GaijinpotJobListing",
+    "parse_gaijinpot_jobs",
+    "scrape_gaijinpot_jobs",
+]
