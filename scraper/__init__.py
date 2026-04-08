3b1ff0e0577b1244bd6e1c840bd752a0ebbdef5f
carlos ledoux <carlosledoux@gmail.com>
2026-04-08 18:33:05 +0900

    TokoyDev Scrapper

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -2,6 +2,7 @@
 
 from .daijob import DaijobJobListing, parse_daijob_jobs, scrape_daijob_jobs
 from .gaijinpot import GaijinpotJobListing, parse_gaijinpot_jobs, scrape_gaijinpot_jobs
+from .tokyodev import TOKYODEV_JOBS_URL, parse_tokyodev_jobs, scrape_tokyodev
 
 __all__ = [
     "DaijobJobListing",
@@ -10,4 +11,7 @@ __all__ = [
     "GaijinpotJobListing",
     "parse_gaijinpot_jobs",
     "scrape_gaijinpot_jobs",
+    "TOKYODEV_JOBS_URL",
+    "parse_tokyodev_jobs",
+    "scrape_tokyodev",
 ]
